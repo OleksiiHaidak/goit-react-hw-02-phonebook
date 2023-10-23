a68a23a3c9143c48c059446a236b0427a76b3023
OleksiiHaidak <xalexey.g@gmail.com>
2023-10-23 22:14:03 +0300

    update ContactList and create ContactListItem

--- src/components/Phonebook/ContactList.jsx
+++ src/components/Phonebook/ContactList.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import css from 'components/Phonebook/Phonebook.module.css';
+import ContactListItem from "./ContactListItem";
 
-const ContactList = ({contacts, onDeleteContact}) => (
-    <div className={css.contactsForm}>
-  <ul className={css.contactList}>
-    {contacts.map(contact => (
-      <li key={contact.id} className={css.contactList}>
-        {contact.name}: {contact.number}
-        <button onClick={() => onDeleteContact(contact.id)} className={css.delBtn}>Delete</button>
-      </li>
-    ))}
-        </ul>
-    </div>
+const ContactList = ({ contacts, onDeleteContact }) => (
+  <div className={css.contactsForm}>
+    <ul className={css.contactList}>
+      {contacts.map(contact => (
+        <ContactListItem key={contact.id} contact={contact} onDeleteContact={onDeleteContact}/>
+      ))}
+    </ul>
+  </div>
 );
 
 export default ContactList;
